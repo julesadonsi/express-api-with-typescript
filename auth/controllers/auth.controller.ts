d7cc9102632a390ac5bcb586ba12a76b78b00648
JulesAD96 <adonsijules9@gmail.com>
2022-08-26 15:25:29 +0100

    Bug fixing with env variable

--- auth/controllers/auth.controller.ts
+++ auth/controllers/auth.controller.ts
@@ -1,31 +1,35 @@
-import express from "express";
-import debug from "debug";
-import jwt from "jsonwebtoken";
+import express from 'express';
+import debug from 'debug';
+import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 
 const log: debug.IDebugger = debug('app:auth-controller');
-const jwtSecret: string = process.env.JWT_SECRET!;
+
+const jwtSecret: string = process.env.JWT_SECRET ?? "My!@!Se3cr8tH4sh3";
 const tokenExpirationInSeconds = 36000;
 
 class AuthController {
     async createJWT(req: express.Request, res: express.Response) {
+        console.log(jwtSecret)
         try {
             const refreshId = req.body.userId + jwtSecret;
             const salt = crypto.createSecretKey(crypto.randomBytes(16));
-            const hash = crypto.createHmac('sha512', salt)
+            const hash = crypto
+                .createHmac('sha512', salt)
                 .update(refreshId)
                 .digest('base64');
-            
             req.body.refreshKey = salt.export();
-            const token = jwt.sign(req.body, jwtSecret, {expiresIn:tokenExpirationInSeconds});
-
-            return res.status(201).send({accessToken:token, refreshToken:hash})
-        } catch (error) {
-            log('CreateJwt error: 0%', error)
+            const token = jwt.sign(req.body, jwtSecret, {
+                expiresIn: tokenExpirationInSeconds,
+            });
+            return res
+                .status(201)
+                .send({ accessToken: token, refreshToken: hash });
+        } catch (err) {
+            log('createJWT error: %O', err);
             return res.status(500).send();
         }
     }
 }
-
 
 export default new AuthController();
